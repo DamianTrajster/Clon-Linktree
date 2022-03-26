4b1ec37f9813bcdec32fd317fa973604dcf873b1
DamianTrajster <74788379+DamianTrajster@users.noreply.github.com>
2022-03-26 19:11:39 -0300

    config rutas

--- src/index.js
+++ src/index.js
@@ -2,16 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+/* Rutas */
+import LoginView from "./routes/LoginView";
+import DashboardView from "./routes/DashboardView";
+import EditProfileView from './routes/EditProfileView';
+import SignOutView from "./routes/SignOutView";
+import PublicProfileView from "./routes/PublicProfileView";
+import ChooserUsernameView from "./routes/ChooserUsernameView";
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}/>
+      <Route path="login" element={<LoginView />}/>
+      <Route path="dashboard" element={<DashboardView />}/>
+      <Route path="dashboard/profile" element={<EditProfileView />}/>
+      <Route path="signout" element={<SignOutView/>}/>
+      <Route path="u/:username" element={<PublicProfileView/>}/>
+      <Route path="choose-username" element={<ChooserUsernameView/>}/>
+    </Routes>
+  </BrowserRouter>,
+
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
